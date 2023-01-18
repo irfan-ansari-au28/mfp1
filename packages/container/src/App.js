dfba5db33e2042895a3868bf090885413d99a590
apple <apple@apples-MacBook-Pro.local>
2023-01-18 15:41:50 +0530

    add mfp

---
+++ packages/container/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import MarketingApp from "./components/MarketingApp";
+
+const App = () => {
+  return (
+    <div>
+      <div>Container App</div>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
+
+export default App;
