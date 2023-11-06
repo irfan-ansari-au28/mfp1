b3b1e1a44daf2fd2212f58a7f7550497bc1c3afd
irfan-ansari-au28 <irfan398649@gmail.com>
2023-11-06 20:37:57 +0530

    bubble-short

---
+++ src/JS/bubble-short.js
@@ -0,0 +1,14 @@
+const arr1 = [1, 3, 2, 5, 4, 6];
+
+const bubbleShort = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
+
+bubbleShort(arr1);
