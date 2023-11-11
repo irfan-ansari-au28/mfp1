e6aee63075b44ddd8267987171c37d86b37ebddb
irfan-ansari-au28 <irfan398649@gmail.com>
2023-11-11 18:25:32 +0530

    different ways to create objects

---
+++ src/JS/Objects/objects.js
@@ -0,0 +1,27 @@
+// var object = new Object();
+
+// var object = Object.create(null);
+
+/* var object = {
+    name: "Sudheer",
+    age: 34
+};
+*/
+
+/* 
+function Person(name) {
+  this.name = name;
+  this.age = 21;
+}
+var object = new Person("Sudheer");
+*/
+
+/*
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+var object = new Person("Sudheer");
+*/
