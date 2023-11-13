2ba400c501146f16a56b1b1a6c1b35d1bcc5d9d9
irfan-ansari-au28 <irfan398649@gmail.com>
2023-11-13 21:57:11 +0530

    fetch users fix

--- src/components/make-request/FetchUsers.js
+++ src/components/make-request/FetchUsers.js
@@ -1,30 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles.css";
 
 export default function App() {
   const [data, setData] = useState([]);
 
   const getUsers = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await response.json();
-    console.log(data);
-    return data;
-  };
-
-  const clickHandler = async (e) => {
     try {
-      const users = await getUsers();
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const users = await response.json();
       setData(users);
-      console.log(users, "data"); // Use 'users' instead of 'data' here
     } catch (error) {
       console.error("Error fetching users:", error);
     }
   };
 
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <button onClick={clickHandler}>Fetch Users</button>
+      <button onClick={getUsers}>Fetch Users</button>
       {data && data.length}
       <h2>Start editing to see some magic happen!</h2>
     </div>
